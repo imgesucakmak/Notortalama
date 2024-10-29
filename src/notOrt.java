1373bf33892c46f7ba02181f08d3a5cdd4d24693
imgesucakmak <cakmakimgesu@gmail.com>
2024-10-29 14:58:46 +0300

    Update notOrt.java

--- src/notOrt.java
+++ src/notOrt.java
@@ -3,29 +3,26 @@ import java.util.Scanner;
 public class notOrt {
 
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		int ogrenciSayisi=0;
+		Scanner scanner = new Scanner(System.in);
+		int ogrenciSayisi = 0;
 		
-		while(ogrenciSayisi>=0) {
-			System.out.println("kac ogrenci not girecek?");
-			ogrenciSayisi=scanner.nextInt();
-			
-			double toplamOrtalama=0;
-			
-			for(int i=1;i<=ogrenciSayisi; i ++) {
-				System.out.println(i+".ogrenci not ort girin:");
-				double notOrtalamasi=scanner.nextDouble();
-				toplamOrtalama=notOrtalamasi;}
-			
-			double genelOrtalama=toplamOrtalama/ogrenciSayisi;
-			System.out.println("girilen öğrencilerin genel not ort:"+genelOrtalama);
-				
-				
-			
-			
+		
+		while (ogrenciSayisi <= 0) {
+			System.out.println("Kaç öğrenci not girecek?");
+			ogrenciSayisi = scanner.nextInt();
 		}
 		
-
-	}
-
+		double toplamOrtalama = 0;
+		
+		for (int i = 1; i <= ogrenciSayisi; i++) {
+			System.out.println(i + ". öğrenci not ortalamasını girin:");
+			double notOrtalamasi = scanner.nextDouble();
+			toplamOrtalama += notOrtalamasi; }
+		
+		double genelOrtalama = toplamOrtalama / ogrenciSayisi;
+		System.out.println("Girilen öğrencilerin genel not ortalaması: " + genelOrtalama); }
 }
+		
+		
+	
+	
